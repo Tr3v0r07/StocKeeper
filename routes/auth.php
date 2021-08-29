90302bcf6789d5c9657c2a53ff16b72f01224899
Tr3v0r07 <81186032+Tr3v0r07@users.noreply.github.com>
2021-08-28 21:27:15 -0500

    test

--- routes/auth.php
+++ routes/auth.php
@@ -11,11 +11,11 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/guymetalshopregistrationlink', [RegisteredUserController::class, 'create'])
-                ->middleware('guest')
-                ->name('register');
+                ->middleware('guest');
+
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
-                ->middleware('guest');
+                ->middleware('guest')->name('register');
 
 Route::get('/login', [AuthenticatedSessionController::class, 'create'])
                 ->middleware('guest')
